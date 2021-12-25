d300945e5a8103ba784dae9f15598ece4feddcbd
maoliqiusiya <981241530@qq.com>
2021-12-26 02:31:51 +0800

    3

---
+++ src/main/java/hk/edu/cuhk/ie/iems5722/a2_1155164913/project/Me.java
@@ -0,0 +1,17 @@
+package hk.edu.cuhk.ie.iems5722.a2_1155164913.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import hk.edu.cuhk.ie.iems5722.a2_1155164913.R;
+
+public class Me extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_me);
+
+    }
+}
