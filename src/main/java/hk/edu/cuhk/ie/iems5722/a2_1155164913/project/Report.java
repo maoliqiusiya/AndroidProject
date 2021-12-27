5fb4f9a5a23b0db1ec979896fdee36bf211e6b00
maoliqiusiya <981241530@qq.com>
2021-12-27 18:37:20 +0800

    report

--- src/main/java/hk/edu/cuhk/ie/iems5722/a2_1155164913/project/Report.java
+++ src/main/java/hk/edu/cuhk/ie/iems5722/a2_1155164913/project/Report.java
@@ -15,10 +15,13 @@ import hk.edu.cuhk.ie.iems5722.a2_1155164913.WiFiMainActivity;
 public class Report extends AppCompatActivity {
 
     private ImageView iv_today;
+    private ImageView iv_month;
+    private ImageView iv_year;
     private RadioButton rbGroup;
     private RadioButton rbWiFi;
     private RadioButton rbReport;
     private RadioButton rbMe;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +31,7 @@ public class Report extends AppCompatActivity {
         rbWiFi = findViewById(R.id.rbContact);
         rbReport = findViewById(R.id.rbSchedule);
         rbMe = findViewById(R.id.rbMe);
+
         rbGroup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -57,11 +61,31 @@ public class Report extends AppCompatActivity {
             }
         });
 
+
         iv_today = findViewById(R.id.it_report_day_iv);
         iv_today.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Report.this, ReportAdding.class);
+                intent.putExtra("name", "Report for today");
+                startActivity(intent);
+            }
+        });
+        iv_month = findViewById(R.id.it_report_month_iv);
+        iv_month.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Report.this, ReportAdding.class);
+                intent.putExtra("name", "Report for month");
+                startActivity(intent);
+            }
+        });
+        iv_year = findViewById(R.id.it_report_year_iv);
+        iv_year.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Report.this, ReportAdding.class);
+                intent.putExtra("name", "Report for year");
                 startActivity(intent);
             }
         });
--- src/main/java/hk/edu/cuhk/ie/iems5722/a2_1155164913/project/ReportAdding.java
+++ src/main/java/hk/edu/cuhk/ie/iems5722/a2_1155164913/project/ReportAdding.java
@@ -1,16 +1,73 @@
 package hk.edu.cuhk.ie.iems5722.a2_1155164913.project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import hk.edu.cuhk.ie.iems5722.a2_1155164913.Chatrooms;
 import hk.edu.cuhk.ie.iems5722.a2_1155164913.R;
+import hk.edu.cuhk.ie.iems5722.a2_1155164913.WiFiMainActivity;
 
 public class ReportAdding extends AppCompatActivity {
 
+    private RadioButton rbGroup;
+    private RadioButton rbWiFi;
+    private RadioButton rbReport;
+    private RadioButton rbMe;
+    private ImageView ivUpload;
+    private String report_name;
+    private TextView page_title;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report_adding);
+        rbGroup = findViewById(R.id.rbGroup);
+        rbWiFi = findViewById(R.id.rbContact);
+        rbReport = findViewById(R.id.rbSchedule);
+        rbMe = findViewById(R.id.rbMe);
+        ivUpload = findViewById(R.id.upload_iv);
+        report_name = this.getIntent().getExtras().getString("name");
+        page_title = findViewById(R.id.report_time);
+        page_title.setText(this.getIntent().getExtras().getString("name"));
+        rbGroup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ReportAdding.this, Chatrooms.class);
+                startActivity(intent);
+            }
+        });
+        rbWiFi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ReportAdding.this, WiFiMainActivity.class);
+                startActivity(intent);
+            }
+        });
+        rbReport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ReportAdding.this, Report.class);
+                startActivity(intent);
+            }
+        });
+        rbMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ReportAdding.this, Me.class);
+                startActivity(intent);
+            }
+        });
+//        ivUpload.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        });
+
     }
 }
