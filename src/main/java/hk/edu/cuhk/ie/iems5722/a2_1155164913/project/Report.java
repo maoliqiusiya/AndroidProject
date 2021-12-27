a6d599864a4ef0f2cbefce4b8b7ec6b886f790d0
maoliqiusiya <981241530@qq.com>
2021-12-27 16:29:42 +0800

    uniform

--- src/main/java/hk/edu/cuhk/ie/iems5722/a2_1155164913/project/Report.java
+++ src/main/java/hk/edu/cuhk/ie/iems5722/a2_1155164913/project/Report.java
@@ -1,6 +1,9 @@
 package hk.edu.cuhk.ie.iems5722.a2_1155164913.project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,9 +11,19 @@ import hk.edu.cuhk.ie.iems5722.a2_1155164913.R;
 
 public class Report extends AppCompatActivity {
 
+    private ImageView iv_today;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
+        iv_today = findViewById(R.id.it_report_day_iv);
+        iv_today.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Report.this, ReportAdding.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ src/main/java/hk/edu/cuhk/ie/iems5722/a2_1155164913/project/ReportAdding.java
@@ -0,0 +1,16 @@
+package hk.edu.cuhk.ie.iems5722.a2_1155164913.project;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import hk.edu.cuhk.ie.iems5722.a2_1155164913.R;
+
+public class ReportAdding extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_adding);
+    }
+}
